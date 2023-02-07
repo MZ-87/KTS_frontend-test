0614f3c316c1cf58852594ad8e089ba058fe56af
MZ-87 <zsmile@mail.ru>
2023-02-07 15:28:24 +0300

    task 2 solved

---
+++ src2.js
@@ -0,0 +1,27 @@
+function curry(f) { // curry(f) выполняет каррирование
+    return function(a) {
+      return function(b) {
+        return f(a, b);
+      };
+    };
+  }
+  
+  // использование
+  const pow = (x, n) => {
+    let result = x;
+    try {
+        if (typeof x !== 'number' || typeof n !== 'number') {
+            throw new Error('INVALID_ARGUMENT');
+        }
+        for (let i = 1; i < n; i++) {
+            result *= x;
+        }
+        return result;
+    } catch (e) {
+        console.log(e);
+    }
+};
+  
+  let curriedPow = curry(pow);
+  console.log(curriedPow(2)(3));
+  console.log(pow(10,3));
