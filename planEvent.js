e0bca5946e54724768dd05961d59e13ad9f71104
MZ-87 <zsmile@mail.ru>
2023-02-09 11:07:12 +0300

    add description of the last task

--- planEvent.js
+++ planEvent.js
@@ -17,3 +17,9 @@ const result = await planEvent(f, 3000); //Через 3 секунды
 console.log(result); // 'Done'
 const result2 = await planEvent('str', 3000); // ошибка с кодом INVALID_ARGUMENT
 const result3 = await planEvent(() => 'Done', '3000'); // ошибка с кодом INVALID_ARGUMENT
+
+
+
+// Реализуйте функцию planEvent, которая планирует вызов функции cb через некоторое 
+// время(timeout) и возвращает Promise с результатом выполнения функции cb. 
+// В случае timeout <= 0 вызов должен произойти сразу же.
