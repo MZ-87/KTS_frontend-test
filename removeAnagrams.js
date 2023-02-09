1f0993663e0c9cdd76ac9d7a17db48959f63c675
MZ-87 <zsmile@mail.ru>
2023-02-09 16:34:04 +0300

    removeAnagrams - need to change compare loop

--- removeAnagrams.js
+++ removeAnagrams.js
@@ -10,29 +10,41 @@ function removeAnagrams(arr) {
       }
     }
 
-    let map = new Map();
     let sortedArr = [];
-    
-    // for (let word of arr) {
-    //   let sorted = word.toLowerCase().split('').sort().join(''); 
-    //   map.set(sorted, word);
-    // }
+    let uniqueArr = [];
 
     for (let i = 0; i < arr.length; i++) {
       let sorted = arr[i].toLowerCase().split('').sort().join('');
+      // console.log(sorted);
       sortedArr.push(sorted);
-      console.log(sortedArr);
-      map.set(sorted, word);
+      // map.set(sorted, word);
     }
-    return Array.from(map.values());
+    console.log(sortedArr);
+    for (let i = 0; i < sortedArr.length; i++) {
+      console.log('loop 1 start');
+      console.log(sortedArr[i]);
+      for (let j = 0; j < sortedArr.length; j++) {
+        console.log('loop 2 start');
+        console.log(sortedArr[j]);
+        if (sortedArr[i] !== sortedArr[j]) {
+          uniqueArr.push(sortedArr[i])
+          console.log(`unique array: ${uniqueArr}`);
+        } else {
+          break;
+        }
+      }
+    }
+    console.log(uniqueArr);
+    return uniqueArr;
   } catch (e) {
     return e;
   }
 }
 
-console.log(removeAnagrams(['cat', 'act', 'arc']));
-console.log(removeAnagrams(['car', 'arc']));
-console.log(removeAnagrams('str'));
-console.log(removeAnagrams(['str', 5]));
+// console.log(removeAnagrams(['cat', 'arc', 'act']));
+console.log(removeAnagrams(['tree', 'eetr', 'map', 'road', 'doar']));
+// console.log(removeAnagrams(['car', 'arc']));
+// console.log(removeAnagrams('str'));
+// console.log(removeAnagrams(['str', 5]));
 
 //Реализуйте функцию removeAnagrams, которая принимает массив строк и удаляет из данного массива строки, являющиеся анаграммами.
